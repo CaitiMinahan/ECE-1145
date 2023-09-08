659f81009f0f1a292595faa2690694714f80ae41
Cameron Henning <cwh35@pitt.edu>
2023-09-08 15:21:07 -0400

    Implemented tiles into the game. Defines what is a plain, ocean, hill, or mountain.

--- src/main/java/hotciv/standard/GameImpl.java
+++ src/main/java/hotciv/standard/GameImpl.java
@@ -30,17 +30,47 @@ import hotciv.framework.*;
 */
 
 public class GameImpl implements Game {
-  public Tile getTileAt( Position p ) { return null; }
+  private Player currentPlayer;
+  public GameImpl() {
+    currentPlayer = Player.RED;
+  }
+  public Tile getTileAt( Position p ) {
+    if ((p.getRow() == 1) && (p.getColumn() == 0)) {
+      return new TileImpl("ocean");
+    } else if (((p.getRow() == 0) && (p.getColumn() == 1))) {
+      return new TileImpl("hills");
+    } else if (((p.getRow() == 2) && (p.getColumn() == 2))) {
+      return new TileImpl("mountain");
+    }
+    else {
+      return new TileImpl("plains");
+    }
+  }
+  private class TileImpl implements Tile {
+      private String terrain;
+      public TileImpl(String terrain){
+        this.terrain = terrain;
+      }
+      @Override
+      public String getTypeString() {
+        return terrain;
+      }
+    }
   public Unit getUnitAt( Position p ) { return null; }
   public City getCityAt( Position p ) { return null; }
-  public Player getPlayerInTurn() { return null; }
+  public Player getPlayerInTurn() {
+    return currentPlayer;
+  }
   public Player getWinner() { return null; }
   public int getAge() { return 0; }
   public boolean moveUnit( Position from, Position to ) {
     return false;
   }
-  public void endOfTurn() {}
+  public void endOfTurn() {
+    currentPlayer = (currentPlayer == Player.RED) ? Player.BLUE : Player.RED;
+  }
   public void changeWorkForceFocusInCityAt( Position p, String balance ) {}
   public void changeProductionInCityAt( Position p, String unitType ) {}
   public void performUnitActionAt( Position p ) {}
+
 }
