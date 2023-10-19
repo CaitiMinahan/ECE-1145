cd5277ec5cad1a7dc4e3beddec4ce9bc4116c97d
cag185 <cag185@pitt.edu>
2023-10-19 12:48:46 -0400

    Refactored code

--- src/main/java/hotciv/standard/GenericUnitAction.java
+++ src/main/java/hotciv/standard/GenericUnitAction.java
@@ -12,6 +12,12 @@ public class GenericUnitAction implements UnitAction{
         // based on the selected unit, make the unit do something
         // generic doesn't take action yet
     }
+
+    @Override
+    public void updateUnitMap(Position from, Position to, Unit unit_from, GameImpl game){
+        game.units.remove(from);
+        game.units.put(to, unit_from);
+    }
     // create a method for moving based on the UnitActionType
     // try to move unit and return true if nothing is there
     // then place the unit at the desired position
@@ -37,15 +43,16 @@ public class GenericUnitAction implements UnitAction{
             {
                 // let the attacking unit remove the defending unit and then successfully move to that tile
                 game.killUnit(to);
-                game.units.remove(from);
-                game.units.put(to, unit_from);
+                updateUnitMap(from, to, unit_from, game);
                 return true;
             }
             return false; // cannot fortify tiles (move own units to tile with own units)
         }
         // otherwise, move the unit from the original position to the new one
-        game.units.remove(from);
-        game.units.put(to, unit_from);
+        updateUnitMap(from, to, unit_from, game);
         return true;
     }
+
+
+
 }
--- src/main/java/hotciv/standard/UnitAction.java
+++ src/main/java/hotciv/standard/UnitAction.java
@@ -7,6 +7,7 @@ import hotciv.framework.Unit;
 // Set up the template to be implemented by the generic and gamma variants
 public interface UnitAction {
     void performAction(UnitImpl currentUnit, Position p, GameImpl currentGame);
-    public boolean moveUnit( Position from, Position to, GameImpl currentGame);
+    boolean moveUnit( Position from, Position to, GameImpl currentGame);
+    void updateUnitMap(Position from, Position to, Unit unit_from, GameImpl currentGame);
 
 }
