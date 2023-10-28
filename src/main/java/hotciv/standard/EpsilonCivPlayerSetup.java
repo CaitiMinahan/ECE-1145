8f924c03b0cf19ff9540768b4357f47d7a4d9641
cag185 <cag185@pitt.edu>
2023-10-28 16:55:32 -0400

    Step 4, added in implementation for epsilonCiv unitAction and PlayerSetup

---
+++ src/main/java/hotciv/standard/EpsilonCivPlayerSetup.java
@@ -0,0 +1,9 @@
+package hotciv.standard;
+import hotciv.framework.*;
+
+public class EpsilonCivPlayerSetup implements PlayerSetup{
+    @Override
+    public void setupPlayer(GameImpl game){
+        // TODO: need to add in the structure to the gameImpl to track the player successful attacks
+    }
+}
