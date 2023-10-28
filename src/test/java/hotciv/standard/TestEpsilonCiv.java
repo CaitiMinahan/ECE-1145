5f60c302c98debdab775cc4dfdcf96159d0ad1eb
cag185 <cag185@pitt.edu>
2023-10-27 22:59:21 -0400

    Last commit before attemping to merge develop in

--- src/test/java/hotciv/standard/TestEpsilonCiv.java
+++ src/test/java/hotciv/standard/TestEpsilonCiv.java
@@ -30,15 +30,51 @@ public class TestEpsilonCiv {
         game = new GameImpl(epsilonCivWorldLayout, epsilonWorldAging, epsilonCivWinner, epsilonUnitAction, epsilonCivPlayerSetup);
     }
 
+    // a test stub for setting the defensive and attacking strengths
+    public class FixedStrengthStrategy implements Unit{
+        // want to return the strengths of the unit
+        @Override
+        public int getAttackingStrength(){
+            return 100;
+        }
+
+        @Override
+        public String getTypeString() {
+            return null;
+        }
+
+        @Override
+        public Player getOwner() {
+            return getO;
+        }
+
+        @Override
+        public int getMoveCount() {
+            return 0;
+        }
+
+        @Override
+        public int getDefensiveStrength() {
+            return 1;
+        }
+    }
+
     @Test
     public void NoCurrentWinnerYieldsNullPlayer(){
         Player winner = game.getWinner();
         assertThat(winner, is(nullValue()));
     }
 
+    // Show that a successful attack increases the player's win count
     @Test
     public void AttackIsResolvedAsWin(){
+        // attacking unit - archer at 0,0 red
+        Position redArcherPos = new Position(0,0);
+        // defending unit - legion at 1,2 blue
+        Position blueLegionPos = new Position(1,2);
 
+        //set attacking strength -- stubs
+        // set defending strength -- stubs
     }
 
     @Test
