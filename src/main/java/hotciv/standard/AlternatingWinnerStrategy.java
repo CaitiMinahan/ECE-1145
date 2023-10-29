46b0782f40fed4c7b06b051eff07d8fb50852a2c
CaitiMinahan <cpm49@pitt.edu>
2023-10-29 17:56:02 -0400

    fixed tests in TestAlternatingWinnerStrategy and fixed the switching code in AlternatingWinnerStrategy

--- src/main/java/hotciv/standard/AlternatingWinnerStrategy.java
+++ src/main/java/hotciv/standard/AlternatingWinnerStrategy.java
@@ -3,22 +3,17 @@ import hotciv.framework.*;
 
 public class AlternatingWinnerStrategy implements Winner{
     private Winner currentWinnerState;
-    private int round;
     public AlternatingWinnerStrategy(){
         this.currentWinnerState = new BetaCivWinner();
-        this.round = 0;
     }
 
     @Override
     public Player gameWinner(GameImpl game) {
-        round++;
 
-        if (round <= 20) {
-            //currentWinnerState = new BetaCivWinner();
+        if (game.getTurnCount() <= 20) {
             return currentWinnerState.gameWinner(game);
         } else {
-            // currentWinnerState = new EpsilonCivWinner();  // TODO: get EpsilonCivWinner when it is ready
-            currentWinnerState = new GenericWinner(); // TODO: remove this. placeholder so I can commit my changes
+            currentWinnerState = new EpsilonCivWinner();
             return currentWinnerState.gameWinner(game);
         }
     }
