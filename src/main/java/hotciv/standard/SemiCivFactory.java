229b1207ac58f0e813a82bca7490219ff88afe95
Cameron Henning <cwh35@pitt.edu>
2023-11-01 15:39:25 -0400

    Added SemiCivFactory and TestSemiCiv, still need to look at EpsilonCiv's tests within TestSemiCiv

---
+++ src/main/java/hotciv/standard/SemiCivFactory.java
@@ -0,0 +1,30 @@
+package hotciv.standard;
+
+public class SemiCivFactory implements GameFactory {
+
+    public UnitAttacking unitAttacking = new GenericUnitAttacking();
+    @Override
+    public WorldLayout createWorldLayout() {
+        // SemiCiv = DeltaCiv layout
+        return new DeltaCivWorldLayout();
+    }
+    @Override
+    public WorldAging createWorldAging() {
+        // SemiCiv = BetaCiv aging
+        return new BetaCivWorldAging();
+    }
+    @Override
+    public Winner createWinnerStrategy() {
+        // SemiCiv = EpsilonCiv winner
+        return new EpsilonCivWinner();
+    }
+    @Override
+    public UnitAction createUnitAction() {
+        // SemiCiv = Epsilon action
+        return new EpsilonCivUnitAction(unitAttacking);
+    }
+    @Override
+    public PlayerSetup createPlayerSetup() {
+        return new GenericPlayerSetup();
+    }
+}
